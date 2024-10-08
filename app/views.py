abbb0a205c1d550ce30ab6398bc24f889d5fa0f9
Antoine Verlyck <a.verlyck@2s-agency.fr>
2024-10-08 16:52:46 +0200

    starting django project

--- app/views.py
+++ app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def landing(request):
+    return render(request, 'landing.html')
+
+def home(request):
+    return render(request, 'home.html')
+
+# from django.http import HttpResponse
+#
+#
+# def index(request):
+#     return HttpResponse("Hello, world. You're at the polls index.")
--- circleCare/settings.py
+++ circleCare/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app.apps.AppConfig',
 ]
 
 MIDDLEWARE = [
@@ -75,9 +76,9 @@ WSGI_APPLICATION = 'circleCare.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+       'ENGINE': 'djongo',
+       'NAME': 'circleCare',
+   }
 }
 
 
